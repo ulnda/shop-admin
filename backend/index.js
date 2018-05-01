56e5330da041ba468fbb07162bbdafca146e5020
d.nagibin <d.nagibin@fun-box.ru>
2018-05-02 03:15:43 +0400

    Add autoloading for routes.

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,31 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
+
+const path = require('path');
+const fs = require('fs');
+
 const auth = require('./middlewares/auth');
 
-const usersRoutes = require('./routes/v1/users');
-const shopsRoutes = require('./routes/v1/shops');
-const itemsRoutes = require('./routes/v1/items');
-const citiesRoutes = require('./routes/v1/cities');
-const newsRoutes = require('./routes/v1/news');
-const servicesRoutes = require('./routes/v1/services');
-const vacanciesRoutes = require('./routes/v1/vacancies');
+const ROUTES_FOLDER = 'routes/v1';
 
 const PORT = 3000;
 
 const app = new Koa();
 
 app.use(koaBody());
-
 app.use(auth);
-app.use(usersRoutes.routes());
-app.use(shopsRoutes.routes());
-app.use(itemsRoutes.routes());
-app.use(citiesRoutes.routes());
-app.use(servicesRoutes.routes());
-app.use(newsRoutes.routes());
-app.use(vacanciesRoutes.routes());
+
+addRoutes();
 
 app.listen(PORT);
+
+function addRoutes() {
+  const routesFolder = path.join(__dirname, ROUTES_FOLDER);
+
+  fs
+    .readdirSync(routesFolder)
+    .forEach(file => {
+      const routesFile = require(path.join(routesFolder, file));
+      app.use(routesFile.routes());
+    });
+}
